ec382bbdcc995771807ddaf599a8adfc60ba8814
Roger Waterson <aznrogerazn@gmail.com>
2014-12-21 16:32:26 +0800

    hackathon progress

--- main.js
+++ main.js
@@ -8,7 +8,29 @@
 
 */
 
+// Declare operational variables
+var toggledMenuName;
+var toggleMenuLayer = 0;
+var ifMenuToggled = 0;
+var toggledFunctionMenu;
+var loginEnabled = 1;
+var chatUpdated = 0;
+var fbLoginUrl;
+var summaryToggled = 0;
+var ifJoinedOp = 0;
+var intelToggled = 0;
+var newIntelToggled = 0;
 
+var deviceModel, deviceOS;
+
+var chatAllowed = 1;
+
+var d = new Date;
+
+var serverHostName = 'assemble.roger.rmstudio.tw';
+var userName, userEmail, userPhotoUrl;
+var motd;
+//var currentUserObject = new UserObj('','','b01102019@ntu.edu.tw','','','');
 
 
 // Do when the window has been successfully loaded
@@ -20,6 +42,60 @@ var pointer = {
 // Declare map object
 var map;
 
+
+// Socket.IO connection start
+
+
+// INIT: Server connection
+var socket = io.connect('https://' + serverHostName + ':6593/world');
+
+
+// SOCKET.IO Processing
+
+socket.on('connect', function() {
+
+
+    // check if no session
+    if (localStorage.token) {
+        socket.emit('continue', localStorage.token, function(ret){
+            if (ret) {
+                switch (ret) {
+                    case 0:
+                        World_show();
+                        break;
+                    case 1:
+                        alert('與伺服器失去連線，請重新登入！')
+                        break;
+                    default:
+                        alert('未知錯誤，請重新登入！');
+                        break;
+                    }
+                    // kick the motherfucker out
+                    navigator.app.exitApp();
+            }
+
+        });
+    }
+
+
+
+
+        socket.on('disconnect', function(){
+
+
+
+        });
+});
+
+
+// Socket.IO connection end
+
+
+
+
+
+
+
 $(window).on('load',function(){
     if ($("#loginWhich").hasClass("show")) {
         $("#loginWhich").removeClass("show");
@@ -29,7 +105,7 @@ $(window).on('load',function(){
     }
 
     // Process device status
-    //document.addEventListener("deviceready", onDeviceReady(), false);
+    document.addEventListener("deviceready", onDeviceReady, false);
 
     // Add pointer locator
     document.addEventListener("mousemove", function(e){
@@ -37,13 +113,19 @@ $(window).on('load',function(){
         pointer.y = e.y;
     });
 
-    // Initialise Google Maps object
-    // var div = document.getElementById("worldMap");
-    // map = plugin.google.maps.Map.getMap(div);
+
+    var i = window.innerHeight - 96;
+    var j = window.innerWidth;
+    $("#worldMap").css("height", i+"px");
+    $("#worldMap").css("width", j+"px");
 
 
-    // Set page heights
-    
+    // OSM
+    map = L.map('worldMap').setView([25.044045,121.519902], 17);
+    L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
+        attribution: 'with &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+        maxZoom: 18
+    }).addTo(map);
 
 
 
@@ -62,8 +144,13 @@ function onDeviceReady(){
     //                'Device Platform: ' + device.platform + '<br />' +
     //               'Device UUID: '     + device.uuid     + '<br />' +
     //               'Device Version: '  + device.version  + '<br />';
-    var deviceModel = device.model;
-    var deviceOS = device.version;
+    deviceModel = device.model;
+    deviceOS = device.version;
+    alert("device ready.");
+
+
+
+
     //alert("This is a " + deviceModel + ", running " + deviceOS);
 
     //socket.emit('debug','Hello from ' + deviceModel + ', Android v' + deviceOS);
@@ -78,9 +165,35 @@ $(document).on('backbutton', function () {
     backHandler();
 });
 
+// Process exit decision
+function tryQuit(){
+    if (confirm("你確定要結束程式嗎？")){
+        navigator.app.exitApp();
+    }
+}
+
 // Process back button behavior
 
 function backHandler(){
+    // determine if it's at level 0
+    if (toggledPage == 0) {
+        tryQuit();
+        return false;
+    }
+
+    // close menu first
+    if (menuToggled){
+        $("#worldMenu").css("-webkit-transform", "translate3d(0px, 0,0)");
+        $("#worldMenuDimmer").css("background", "rgba(0,0,0,0)");
+        $("#worldMenuDimmer").css("pointer-events", "none");
+        menuToggled = false;
+    } else {
+        // switch to map page
+        pageToggle(0);
+
+
+    }
+
 
 }
 
@@ -156,43 +269,79 @@ function showWorld(){
     $("#loginBg").css("opacity","0.0");
     setTimeout(function(){
         $("#loginBg").remove();
-        $("#worldWindow").removeClass("hideAll");
+
+        // INIT: Bind map object to GMaps
+        /*
+        map = new GMaps({
+            div: "#worldMap",
+            lat: 25.044045,
+            lng: 121.519902,
+            zoom: 17,
+            zoomControl: true,
+
+            zoomControlOptions: {
+                style: google.maps.ZoomControlStyle.SMALL
+            },
+            disableDoubleClickZoom: true,
+            mapTypeControl: false,
+            scaleControl: true,
+            scrollwheel: true,
+            panControl: false,
+            streetViewControl: false,
+            draggable : true,
+            overviewMapControl: false,
+            overviewMapControlOptions: {
+                opened: false,
+            },
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        });
+        // INIT: Map styling
+        map.addStyle({
+            styledMapName:"Ground Zeroes",
+            styles: mapOptions,
+            mapTypeId: "map_gz"  });
+
+            map.setStyle("map_gz");
+            alert("map loaded"); */
+
+
     }, 350);
-
-
-    /*
-    map.setOptions({'backgroundColor': '#3d4647','controls': {
-        'compass': true,
-        'myLocationButton': true,
-        'indoorPicker': false,
-        'zoom': true // Only for Android
-    },
-    'gestures': {
-        'scroll': true,
-        'tilt': true,
-        'rotate': true,
-        'zoom': true
-    }});
-    var lel = new plugin.google.maps.LatLng(25.043916, 121.519544);
-    map.moveCamera({
-        'target': lel,
-        'zoom' : 17
-    });
-
-    */
-
-
 
 }
 
 // Process pages
-
+var toggledPage = 0;
 function pageToggle(arg){
+    // defocus
+    document.getElementById('worldChatInput').blur();
     // Move the highlighter
     var i = (window.innerWidth / 5);
     $("#worldFooterSelection").css("-webkit-transform", "translate3d(" + (arg * i) + "px, 0,0)");
-    $("#worldPages").css("-webkit-transform", "translate3d(-" + (arg * window.innerWidth) + "px, 0,0)");
     // Move the page
+    $("#worldPages").css("-webkit-transform", "translate3d(-" + (arg * window.innerWidth) + "px, 0,0)");
+    // Change the title
+    switch (arg) {
+        case 0:
+            $("#worldBannerTitle").text("地圖總覽");
+            toggledPage = 0;
+            break;
+        case 1:
+            $("#worldBannerTitle").text("作戰行動");
+            toggledPage = 1;
+            break;
+        case 2:
+            $("#worldBannerTitle").text("我的小隊");
+            toggledPage = 2;
+            break;
+        case 3:
+            $("#worldBannerTitle").text("情報");
+            toggledPage = 3;
+            break;
+        case 4:
+            $("#worldBannerTitle").text("即時通訊");
+            toggledPage = 4;
+            break;
+    }
 }
 
 
@@ -202,9 +351,13 @@ var menuToggled = false;
 function menuToggle(){
     if (menuToggled){
         $("#worldMenu").css("-webkit-transform", "translate3d(0px, 0,0)");
+        $("#worldMenuDimmer").css("background", "rgba(0,0,0,0)");
+        $("#worldMenuDimmer").css("pointer-events", "none");
         menuToggled = false;
     } else {
         $("#worldMenu").css("-webkit-transform", "translate3d(104%, 0,0)");
+        $("#worldMenuDimmer").css("background", "rgba(22,22,22,0.73)");
+        $("#worldMenuDimmer").css("pointer-events", "auto");
         menuToggled = true;
     }
 }
